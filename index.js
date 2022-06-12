cbc040fd483c5fb63ade3dd25d31055febfe0b74
Adaobi Osakwe <adaobiosakwe@Adaobis-MacBook-Air.local>
2022-06-12 17:43:36 +0100

    update mmd files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let gallery = [
+    {id: 9, type: 'img', src: "./assets/campaign-images/2.jpeg", language: "hausa"},
+    {id: 8, type: 'img', src: "https://pbs.twimg.com/media/FUb7hrgX0AEwePq?format=jpg&name=medium", language: "eng"},
     {id: 1, type: 'tags', src: "#OBIdent", language: "eng"},
     {id: 2, type: 'img', src: "https://moneycentral.com.ng/wp-content/uploads/2022/06/Peter-Obi-2023.jpg", language: "eng"},
+    {id: 9, type: 'img', src: "https://pbs.twimg.com/media/FVBUy0gWUAAOBIQ?format=jpg&name=medium", language: "eng"},
     {id: 3, type: 'img', src: "https://www.theafricareport.com/media/2022/05/obi-e1651492034434-732x419.jpeg", language: "eng"},
     {id: 4, type: 'tags', src: "#PeterObi4President", language: "eng"},
-    {id: 5, type: 'tags', src: "#TakeBackaija", language: "eng"},
+    {id: 5, type: 'tags', src: "#TakeBackNaija", language: "eng"},
     {id: 6, type: 'audio', src: "/media/cc0-audio/t-rex-roar.mp3", language: "eng"},
-    {id: 6, type: 'video', src: "https://www.youtube.com/watch?v=Z9J6KA9prb0", language: "eng"},
+    {id: 7, type: 'video', src: "https://twitter.com/i/status/1532629465070706688", language: "eng"},
 ]
 
 let gallerylist  = document.getElementsByClassName("content-gallery")[0];
@@ -14,14 +17,21 @@ function displayGallery(list){
     for(let i=0; i < list.length; i++){
         let val = list[i]
         if(val.type === "img"){
-            gallerylist.innerHTML += `<div class="gallery">
+            gallerylist.innerHTML += 
+            `<div class="gallery">
                 <img class="img-gallery" src=${val.src} alt="img-gallery" />
                 <div class="share-container">
+                <a download=${`peterobi4president` + val.id} href=${val.src}>
                     <img  class="icon" src="./assets/download.png" alt="share" />
+                </a>
                     <div>
                         <img onclick=copyText("${val.src}")  class="icon" src="./assets/share.png" alt="share" />
                         <span> 
-                            <img class="minicons" src="./assets/twitter.png" alt="twitter" />
+                            <a target="_blank" class="twitter-share-button"
+                                href="https://twitter.com/intent/tweet?url=${val.src}">
+                                <img class="minicons" src="./assets/twitter.png" alt="twitter" />
+                            </a>
+                            
                             <img class="minicons" src="./assets/facebook.png" alt="facebook" />
                             <img class="minicons" src="./assets/instagram.png" alt="instagram" />
                             <img class="minicons" src="./assets/whatsapp.png" alt="instagram" />
@@ -77,7 +87,10 @@ function displayGallery(list){
                 <img onclick=copyText("${val.src}") class="icon" src="./assets/share.png" alt="share" />
                 <div>    
                     <span> 
-                        <img class="minicons" src="./assets/twitter.png" alt="twitter" />
+                    <a target="_blank" class="twitter-share-button"
+                    href="https://twitter.com/intent/tweet?text="some text"&hashtags=${val.src}">
+                    <img class="minicons" src="./assets/twitter.png" alt="twitter" />
+                </a>
                         <img class="minicons" src="./assets/facebook.png" alt="facebook" />
                         <img class="minicons" src="./assets/instagram.png" alt="instagram" />
                         <img class="minicons" src="./assets/whatsapp.png" alt="instagram" />
@@ -106,3 +119,9 @@ function filteroptions(type){
     let filtered = gallery.filter(all => all.type === type);
     displayGallery(filtered);
 }
+
+//todo list
+//share functionalities
+//give people credit to encourage people to contribute
+//add option to contribute
+//filter by language
