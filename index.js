7022dd069e2c1b3ea21d1dbd018bce66cdc1f284
Adaobi Osakwe <adaobiosakwe@Adaobis-MacBook-Air.local>
2022-06-17 21:34:08 +0100

    update campaign materials

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let gallery = [
-    
+    {id: 7, type: 'video', src: "./assets/campaign-videos/j1.mp4", language: "eng", link: "https://youtu.be/raWPpg6tvbw"},
+    {id: 14, type: 'img', src: "./assets/campaign-images/7.jpeg", language: "eng"}, 
+    {id: 15, type: 'img', src: "./assets/campaign-images/8.jpeg", language: "eng"},
+    {id: 16, type: 'img', src: "./assets/campaign-images/9.jpeg", language: "eng"},
+    {id: 17, type: 'img', src: "./assets/campaign-images/10.jpeg", language: "eng"},
+    {id: 18, type: 'img', src: "./assets/campaign-images/11.jpeg", language: "eng"},
+    {id: 19, type: 'img', src: "./assets/campaign-images/12.jpeg", language: "eng"},
     {id: 10, type: 'img', src: "./assets/campaign-images/4.jpeg", language: "eng"},
     {id: 11, type: 'img', src: "./assets/campaign-images/2.jpeg", language: "hausa"},
     {id: 8, type: 'img', src: "./assets/campaign-images/3.jpeg", language: "eng"},
@@ -10,8 +16,9 @@ let gallery = [
     {id: 4, type: 'tags', src: "PeterObi4President", language: "eng"},
      {id: 5, type: 'tags', src: "TakeBackNaija", language: "eng"},
     // {id: 6, type: 'audio', src: "/media/cc0-audio/t-rex-roar.mp3", language: "eng"},
-    // {id: 7, type: 'video', src: "https://twitter.com/i/status/1532629465070706688", language: "eng"},
     {id: 13, type: 'img', src: "./assets/campaign-images/6.jpeg", language: "eng"},
+   
+
 ]
 
 let gallerylist  = document.getElementsByClassName("content-gallery")[0];
@@ -37,13 +44,14 @@ function displayGallery(list){
             gallerylist.innerHTML += ` <div class="gallery">
             <video width="320" height="240" class="img-gallery" controls>
                 <source src=${val.src} type="video/mp4">
-                <source src=${val.src} type="video/ogg">
               Your browser does not support the video tag.
             </video>
             <div class="share-container">
-            <img  class="icon" src="./assets/download.png" alt="share" />
+            <a download=${`peterobi4president` + val.id} href=${val.src}>
+                <img  class="icon" src="./assets/download.png" alt="share" />
+            </a>
             <div>
-                <img onclick=copyText("${val.src}")  class="icon" src="./assets/share.png" alt="share" />
+                <img onclick=copyText("${val.link}")  class="icon" src="./assets/share.png" alt="share" />
             </div>
         </div>
         </div> `
